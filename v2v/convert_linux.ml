f8009b555f9fc937effa2ae891101b08ddef113c
Hilko Bengen <bengen@debian.org>
2014-09-22 20:06:08 +0200

    Imported Upstream version 1.27.50

--- v2v/convert_linux.ml
+++ v2v/convert_linux.ml
@@ -67,9 +67,6 @@ let rec convert ~verbose ~keep_serial_console (g : G.guestfs) inspect source =
    * an Enterprise Linux guest using RPM.
    *)
 
-  (* We use Augeas for inspection and conversion, so initialize it early. *)
-  Lib_linux.augeas_init verbose g;
-
   (* Basic inspection data available as local variables. *)
   assert (inspect.i_type = "linux");
 
@@ -81,6 +78,14 @@ let rec convert ~verbose ~keep_serial_console (g : G.guestfs) inspect source =
     | _ -> assert false in
 
   assert (inspect.i_package_format = "rpm");
+
+  (* We use Augeas for inspection and conversion, so initialize it early. *)
+  Lib_linux.augeas_init verbose g;
+
+  (* Clean RPM database.  This must be done early to avoid RHBZ#1143866. *)
+  let dbfiles = g#glob_expand "/var/lib/rpm/__db.00?" in
+  let dbfiles = Array.to_list dbfiles in
+  List.iter g#rm_f dbfiles;
 
   (* What grub is installed? *)
   let grub_config, grub =
@@ -393,13 +398,6 @@ let rec convert ~verbose ~keep_serial_console (g : G.guestfs) inspect source =
       )
 
     | `Grub2 -> () (* Not necessary for grub2. *)
-
-  and clean_rpmdb () =
-    (* Clean RPM database. *)
-    assert (inspect.i_package_format = "rpm");
-    let dbfiles = g#glob_expand "/var/lib/rpm/__db.00?" in
-    let dbfiles = Array.to_list dbfiles in
-    List.iter g#rm_f dbfiles
 
   and autorelabel () =
     (* Only do autorelabel if load_policy binary exists.  Actually
@@ -1333,7 +1331,6 @@ let rec convert ~verbose ~keep_serial_console (g : G.guestfs) inspect source =
   in
 
   augeas_grub_configuration ();
-  clean_rpmdb ();
   autorelabel ();
 
   unconfigure_xen ();
