f75496c0bd25616c7eda05e71cf81987419d0c89
Hilko Bengen <bengen@debian.org>
2014-10-03 15:57:03 +0200

    Fixed FTBFS on architectures without zfs-fuse

--- debian/gen-appliance-depends.sh
+++ debian/gen-appliance-depends.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 pkglist_to_deps() {
     echo -n 'appliance:Depends='
--- debian/split-appliance.sh
+++ debian/split-appliance.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 cd debian/tmp/usr/lib/*-*/guestfs/supermin.d
 cp -n packages packages.orig
 
 for p in gfs2 hfsplus jfs nilfs reiserfs rsync xfs zfs; do
     sed -i -e "/$p/d" packages
-    grep -F  "$p" < packages.orig > packages-$p
+    grep -F  "$p" < packages.orig > packages-$p || true
 done
 
 sed -i -e "/^\(iputils\|lsof\|openssh-server\|strace\|vim\)/d" packages
