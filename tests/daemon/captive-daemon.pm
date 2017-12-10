50c3ce909d1a66e49eac18c642e2246e3d813831
Hilko Bengen <bengen@debian.org>
2017-12-10 01:14:27 +0100

    New upstream version 1.36.11

--- tests/daemon/captive-daemon.pm
+++ tests/daemon/captive-daemon.pm
@@ -30,8 +30,8 @@ $| = 1;
 # Filled in by autoconf.
 my %var;
 $var{top_builddir} = "../..";
-$var{abs_top_srcdir} = "/tmp/goaljobstmpf1d73ad3f48f34e2a37340da0aac51fe/libguestfs";
-$var{abs_top_builddir} = "/tmp/goaljobstmpf1d73ad3f48f34e2a37340da0aac51fe/libguestfs";
+$var{abs_top_srcdir} = "/tmp/goaljobstmp20f3a526eb4e10d8db476e5f450acc7f/libguestfs";
+$var{abs_top_builddir} = "/tmp/goaljobstmp20f3a526eb4e10d8db476e5f450acc7f/libguestfs";
 $var{VALGRIND} = "valgrind";
 
 # Now we have to substitute the above variables into this one:
